1790cb64dcd2c4aa254e3f3b25d84854033fd164
Chansik Yun <chansigi@gmail.com>
2023-02-16 13:23:15 +0900

    .

--- 2_변수2.c
+++ 2_변수2.c
@@ -13,9 +13,7 @@
 // long / unsigned long
 // long long / unsigned long long
 
-// float
-// double
-
+#if 0
 #include <stdint.h>
 
 int main()
@@ -23,7 +21,35 @@ int main()
   uint32_t a;
   int8_t b;
 
-  printf("sizeof(long): %lu\n", sizeof(long));
+  printf("sizeof(long): %zu\n", sizeof(long));
+
+  return 0;
+}
+#endif
+
+// - 부동 소수점 타입
+// float(4)
+// double(8)
+// long double(8)
+//
+// => 연산에 오차가 존재한다.
+// => 부동 소수점의 동등성 판단은 오차범위 이내에 들어오는지 여부를
+//    통해 판단합니다.
+
+#include <math.h>
+
+int main(void)
+{
+  printf("sizeof(long double): %zu\n", sizeof(long double));
+
+  double a = 0.7;
+  double b = 0.1 * 7;
+
+  // if (a == b)
+  if (fabs(a - b) < 0.0000001)
+  {
+    printf("같다...\n");
+  }
 
   return 0;
 }

8e9106ace3c16cac3c99a0ca2cb9ff6421edd8fb
Chansik Yun <chansigi@gmail.com>
2023-02-16 14:52:29 +0900

    .

--- 3_상수.c
+++ 3_상수.c
@@ -21,6 +21,12 @@ const int global = 1024;
 
 int main(void)
 {
+  const int n;
+  // 주의
+  // - 초기화 이후로 값을 변경할 수 없기 때문에,
+  //   반드시 초기화해야 합니다.
+  // => C++은 초기화하지 않는 경우, 컴파일 오류가 발생합니다.
+
   // global = 1000;
   printf("%d\n", global);
 

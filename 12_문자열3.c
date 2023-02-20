2884e371c15c5be92a0baab12134f8c62b8b16f3
Chansik Yun <chansigi@gmail.com>
2023-02-20 16:18:51 +0900

    .

--- 12_문자열3.c
+++ 12_문자열3.c
@@ -55,3 +55,80 @@ int main(void)
   return 0;
 }
 #endif
+
+#if 0
+int main(void)
+{
+  const char *str1 = "hello";
+  const char *str2 = "hello";
+
+  if (str1 == str2)
+  {
+    printf("동일 문자열입니다..\n");
+  }
+
+  return 0;
+}
+#endif
+
+// 2. 문자열 비교 함수: strcmp
+#if 0
+//  int strcmp(const char *s1, const char *s2);
+//  - 반환값
+//    1) 같을 경우: 0
+//    2) s1이 s2보다 사전적으로 앞에 위치한다면, 음수(-1)
+//                          뒤에 위치한다면, 양수(1) 을 반환합니다.
+
+int main(void)
+{
+  printf("%d\n", strcmp("abc", "bcd")); // -1
+  printf("%d\n", strcmp("bcd", "abc")); // 1
+
+  char str1[32] = "hello";
+  const char *str2 = "hello";
+
+  // if (str1 == str2)
+  if (strcmp(str1, str2) == 0)
+  {
+    printf("동일 문자열입니다..\n");
+  }
+
+  return 0;
+}
+#endif
+
+// 3. 문자열 복사: strcpy
+//  char * stpcpy(char *dst, const char *src);
+
+//  주의할점
+//   : 복사되는 메모리에 충분히 메모리가 확보되어야 합니다.
+
+#if 0
+int main(void)
+{
+  char str[32]; // 초기화
+  // str = "hello"; // 대입은 허용되지 않습니다.
+
+  strcpy(str, "hello");
+  printf("%s\n", str);
+
+  return 0;
+}
+#endif
+
+// 4. 문자열 연결: strcat
+//  => 주의: 메모리 공간 충분히 확보해야 합니다.
+int main(void)
+{
+  const char *str1 = "hello";
+  const char *str2 = "world";
+
+  char buf[64];
+  strcpy(buf, str1); // "hello"
+
+  strcat(buf, str2); // "helloworld"
+
+  printf("%s\n", buf);
+
+  return 0;
+}

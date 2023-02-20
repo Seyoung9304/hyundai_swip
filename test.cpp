60cf3cf847d890e20e4215945b29adaa698b07f8
Chansik Yun <chansigi@gmail.com>
2023-02-20 13:49:57 +0900

    .

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+  int n = 100;
+
+  void *p = &n;
+  // int*에서 void*로의 암묵적인 변환이 허용됩니다.
+
+  int *pn = (int *)p;
+  // void*에서 int*로의 암묵적인 변환이 허용되지 않습니다.
+
+  printf("%d\n", *pn);
+
+  return 0;
+}

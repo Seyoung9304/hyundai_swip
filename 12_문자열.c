21a9b934d5405d8780256b8213a600f282964af5
Chansik Yun <chansigi@gmail.com>
2023-02-20 15:29:52 +0900

    .

---
+++ 12_문자열.c
@@ -0,0 +1,106 @@
+// 12_문자열.c
+#include <stdio.h>
+
+// char: 문자 타입 => ASCII
+//   A: 65
+//   a: 97
+//   0: 48
+
+// C의 문자열
+//  : char 배열인데, 널문자로 종료되어야 합니다.
+#if 0
+int main(void)
+{
+  char c = 'A';
+  c = 66;
+  printf("%c\n", c);
+
+  printf("%d\n", c); // 65
+
+  return 0;
+}
+#endif
+
+#if 0
+int main(void)
+{
+  // char str[10] = {'h', 'e', 'l', 'l', 'o', '\0'};
+  char str[10] = "hello";
+
+  printf("%s", str);
+
+  return 0;
+}
+#endif
+
+#if 0
+int main(void)
+{
+  char str[6] = "hello"; /* 미정의 동작 */
+  // => 문자열의 마지막은 널문자로 종료되어야 합니다.
+  //    문자열을 배열에 저장할 때, 널문자(1) 크기만큼이 더 필요합니다.
+
+  char buf[1024];
+  printf("%s\n", buf);
+
+  return 0;
+}
+#endif
+
+#if 0
+int main(void)
+{
+  char str[32];
+
+  // scanf는 버퍼의 크기 정보를 무시하고 입력 받기 때문에, 위험합니다.
+  // scanf("%s", str);
+
+  fgets(str, sizeof(str), stdin);
+
+  printf("str: %s\n", str);
+
+  return 0;
+}
+#endif
+
+// 입력 받은 문자열을 소문자면 대문자로 변경하고, 대문자면 소문자로 변경하는
+// 프로그램을 만들어봅시다.
+
+int main(void)
+{
+  char buf[1024];
+
+  // 'a': 97 , 'z': 122
+  // 'A': 65 , 'Z': 90
+
+  int ret;
+  while ((ret = scanf("%s", buf)) == 1)
+  {
+    char *p = buf;
+    while (*p) /* *p != '\0' */
+    {
+      if (*p >= 'A' && *p <= 'Z')
+      {
+        printf("대문자\n");
+        *p = *p + ('a' - 'A');
+      }
+      else if (*p >= 'a' && *p <= 'z')
+      {
+        printf("소문자\n");
+        *p = *p - ('a' - 'A');
+      }
+      else if (*p >= '0' && *p <= '9')
+      {
+        printf("숫자\n");
+      }
+
+      p++;
+    }
+
+    printf("%s\n", buf);
+  }
+
+  printf("Program end..\n");
+
+  return 0;
+}

ba6e8bf4fd530973dd9910d1f835d239201acaf9
Chansik Yun <chansigi@gmail.com>
2023-02-21 14:59:06 +0900

    .

--- 14_함수포인터.c
+++ 14_함수포인터.c
@@ -37,6 +37,19 @@ int main(void)
   printf("result: %d\n", result);
   printf("%p\n", fp);
 
+  int (*fp2)(int, int);
+
+  // 함수 포인터에 주소를 담고, 호출하는 방법 2가지
+  // 1) C++ 선호 스타일
+  fp2 = &add;
+  result = (*fp2)(10, 20);
+
+  // 2) C 선호 스타일
+  // 함수 - decay
+  //  함수의 이름은 함수의 시작 주소로 해석됩니다.
+  fp2 = add;
+  result = fp2(10, 20);
+
   return 0;
 }
 

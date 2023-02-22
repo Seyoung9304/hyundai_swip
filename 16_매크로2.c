c890f0df65b0cfdb5655f503a6941126bb4a9dc1
Chansik Yun <chansigi@gmail.com>
2023-02-22 14:45:28 +0900

    .

--- 16_매크로2.c
+++ 16_매크로2.c
@@ -1,19 +1,25 @@
 // 16_매크로2.c
 #include <stdio.h>
 
-#define SQUARE(x) (x) * (x)
+#define SQUARE(x) ((x) * (x))
 // 1. 매크로 함수는 대문자로 표현하는 것이 일반적입니다.
 
 // 2. 매크로 함수 문제점
 //   1) 우선순위 문제
 //       SQUARE(x+x)
 //       => (x+x) * (x+x)
+//       DBL(x) + DBL(x)
+//       => ((x) + (x)) * ((x) + (x))
+
+#define DBL(x) ((x) + (x))
 
 int main(void)
 {
   int x = 10;
   printf("%d\n", SQUARE(x)); // 100
   printf("%d\n", SQUARE(x + x));
+
+  printf("%d\n", DBL(x) * DBL(x));
 
   return 0;
 }

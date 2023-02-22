30b4b9f36a588e12c393deda79d0348cef3e06be
Seyoung9304 <seftware99@gmail.com>
2023-02-22 12:47:21 +0900

    [feat] quick sort

--- 14_함수포인터4.c
+++ 14_함수포인터4.c
@@ -13,6 +13,35 @@ typedef struct user
   int age;
 } USER;
 
+int cmp_name(const void *a, const void *b)
+{
+  USER ia = *(const USER *)a;
+  USER ib = *(const USER *)b;
+
+  char* namea = ia.name;
+  char* nameb = ib.name;
+  
+  for (int i=0;i<32;i++){
+    if (namea[i]<nameb[i]){
+      return -1;
+    }else if (namea[i]>nameb[i]){
+      return 1;
+    }else{
+      continue;
+    }
+  }
+}
+
+int cmp_age(const void *a, const void *b)
+{
+  double ia = *(const double *)a;
+  double ib = *(const double *)b;
+  // return ia - ib;
+  return ia < ib ? -1 : 1;
+}
+
+#include <stdlib.h>
+
 int main(void)
 {
   USER users[5] = {
@@ -23,8 +52,15 @@ int main(void)
       {"Yuna", 24}};
 
   // 1) qsort를 이용해서, 사전 이름 순서대로 정렬해보세요.
+  qsort(users, 5, sizeof(USER), cmp_name);
+  
+  for (int i = 0; i < 5; i++)
+  {
+    printf("%s(%d)\n", users[i].name, users[i].age);
+  }
 
   // 2) qsort를 이용해서, 내림차순 나이 순서대로 정렬해보세요.
+  qsort(users, 5, sizeof(USER), cmp_age);
 
   for (int i = 0; i < 5; i++)
   {

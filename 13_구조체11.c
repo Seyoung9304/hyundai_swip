10f40c2933697c1a1f60e8237db8478177dcd0d6
Chansik Yun <chansigi@gmail.com>
2023-02-22 10:18:54 +0900

    .

--- 13_구조체11.c
+++ 13_구조체11.c
@@ -32,14 +32,47 @@ void del_user(struct user *user)
   user->age = 0;
 }
 
+void copy_user(struct user *dest, const struct user *src)
+{
+  if (dest->name)
+  {
+    free(dest->name);
+  }
+
+  if (src->name)
+  {
+    dest->name = (char *)malloc(strlen(src->name) + 1);
+    strcpy(dest->name, src->name);
+  }
+  else
+  {
+    dest->name = NULL;
+  }
+
+  dest->age = src->age;
+}
+
 int main(void)
 {
   struct user user1;
   init_user(&user1, "Tom", 42);
 
+  // struct user user2 = user1;
+  // 얕은 복사(Shallow Copy) 문제
+  // > 구조체의 멤버 데이터 중 포인터 멤버가 존재할 경우,
+  //   메모리 복사를 사용할 때 발생합니다.
+  //  - 깊은 복사(Deep Copy)
+
+  struct user user2 = {0};
+  init_user(&user2, "Bob", 10);
+
+  copy_user(&user2, &user1); // 깊은 복사
+
   print_user(&user1);
+  print_user(&user2);
 
   del_user(&user1);
+  del_user(&user2);
 
   return 0;
 }

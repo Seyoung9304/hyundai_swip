eae23535fbf9a30df70d5ce1f587bec3842b52b7
Chansik Yun <chansigi@gmail.com>
2023-02-22 13:09:37 +0900

    .

---
+++ 15_파일입출력2.c
@@ -0,0 +1,40 @@
+// 15_파일입출력2.c
+#include <stdio.h>
+
+#if 0
+int main(void)
+{
+  int num;
+  int sum = 0;
+
+  while (scanf("%d", &num) == 1)
+  {
+    sum += num;
+  }
+
+  printf("sum: %d\n", sum);
+
+  return 0;
+}
+#endif
+
+int main(void)
+{
+  FILE *fp = fopen("b.txt", "rt");
+  if (fp == NULL)
+  {
+    return 1;
+  }
+
+  int num;
+  int sum = 0;
+
+  while (fscanf(fp, "%d", &num) == 1)
+  {
+    sum += num;
+  }
+
+  printf("sum: %d\n", sum);
+
+  return 0;
+}

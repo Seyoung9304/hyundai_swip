bd290a25736045233c535b098b97240544fb3af0
Chansik Yun <chansigi@gmail.com>
2023-02-23 10:20:01 +0900

    .

---
+++ cpp/2_namespace3.cpp
@@ -0,0 +1,14 @@
+// 2_namespace3.cpp
+#include <iostream>
+// 1. C++ 헤더파일은 확장자를 사용하지 않는 경우가 많습니다.
+// 2. 표준 라이브러리는 std 이름공간안에 존재합니다.
+
+// #include <stdio.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+int main()
+{
+    std::printf("Hello, C++\n");
+}

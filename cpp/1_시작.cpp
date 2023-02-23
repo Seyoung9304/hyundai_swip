9cd40c80b865e8cb2aecb3b2b21ba894467bec44
Chansik Yun <chansigi@gmail.com>
2023-02-23 09:28:41 +0900

    .

--- cpp/1_시작.cpp
+++ cpp/1_시작.cpp
@@ -33,7 +33,32 @@ int main()
 // C++의 표준 입출력 헤더파일 입니다.
 //  Input Output Stream
 
+// 표준 출력: std::cout -> stdout
+// 표준 입력: std::cin  -> stdin
+#if 0
 int main()
 {
-    std::cout << "Hello, C++" << std::endl;
+    // std::cout << "Hello, C++" << std::endl;
+
+    int n = 42;
+    double d = 3.14;
+    char c = 'A';
+
+    printf("%d %lf %c\n", n, d, c);
+
+    // - 서식을 지정하지 않아도 타입에 따라서 원하는 형태로
+    //   처리됩니다.
+    std::cout << n << ", "
+              << d << ", " << c << std::endl;
+}
+#endif
+
+int main()
+{
+    double n;
+    // scanf("%d", &n);
+    // printf("%d\n", n);
+
+    std::cin >> n;
+    std::cout << n << std::endl;
 }

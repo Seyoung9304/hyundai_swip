288b21a5568305df94dcbe9bdf04ba57d33ac02f
Chansik Yun <chansigi@gmail.com>
2023-02-23 13:53:02 +0900

    .

--- cpp/a.cpp
+++ cpp/a.cpp
@@ -1,4 +1,21 @@
 // a.cpp
+
+// __Z3fooi
+// void foo(int n) { }
+
+// __Z3foov
+// void foo(void) { }
+
+// __Z3food
+void foo(double d) { }
+
+void goo(int a, int b, char c, double d, long e) { }
+
+int main()
+{
+}
+
+#if 0
 namespace audio {
 void init() { }
 }
@@ -6,3 +23,4 @@ void init() { }
 int main()
 {
 }
+#endif

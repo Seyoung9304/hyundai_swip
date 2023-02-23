7f28b00af491d31b5a676fd95ec8d0e8cc5b64aa
Chansik Yun <chansigi@gmail.com>
2023-02-23 11:24:12 +0900

    .

--- cpp/4_변수4.cpp
+++ cpp/4_변수4.cpp
@@ -5,11 +5,13 @@ using namespace std;
 // auto - C++11
 //  1. '컴파일 타임'에 변수의 타입을 우항의 타입과 동일한 타입으로 타입을 결정합니다.
 //  2. C의 auto라는 키워드를 다른 의미로 사용합니다.
-
 int add(int a, int b) { return a + b; }
 
 int main()
 {
+    auto n = 10;
+    auto d = 3.14;
+
     int x2[3][2] = { 10, 20, 30 };
     // int (*px)[2] = x2;
     auto px = x2;

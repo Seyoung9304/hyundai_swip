4eeb2100186eb345cf8f573d6d8fed1f9f023de2
Chansik Yun <chansigi@gmail.com>
2023-02-23 09:42:08 +0900

    .

---
+++ cpp/a.c
@@ -0,0 +1,17 @@
+// a.c
+
+// int global = 0;
+extern int global;
+extern void foo(void);
+// void foo(void) { }
+
+int video_count = 0;
+void video_init(void)
+{
+}
+
+int main(void)
+{
+
+    return 0;
+}

c6d7f4c824d03c819f61085bde415e00979e2c58
Seyoung9304 <seftware99@gmail.com>
2023-02-23 12:43:38 +0900

    [docs] add comment

--- cpp/5_using.cpp
+++ cpp/5_using.cpp
@@ -27,7 +27,12 @@ PARR3 foo()
 int add(int a, int b) { return a + b; }
 
 // typedef int (*FP)(int, int);
-using FP = int (*)(int, int);
+using FP = int (*)(int, int); 
+/*
+FP는 int()(int, int)의 별칭(alias)입니다. 
+int()(int, int)는 두 개의 int형 매개변수를 받아들이고 int형 값을 반환하는 함수 포인터입니다. 
+따라서, FP는 int형 매개변수 두 개를 받아들이고 int형 값을 반환하는 함수 포인터의 별칭으로 정의되는 것입니다.
+*/
 
 int (*goo())(int, int)
 {

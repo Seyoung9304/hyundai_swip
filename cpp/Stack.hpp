4ee2e3def48c6c1325857d928d60027bcd1b03b0
Chansik Yun <chansigi@gmail.com>
2023-02-24 15:50:51 +0900

    .

--- cpp/Stack.hpp
+++ cpp/Stack.hpp
@@ -2,6 +2,49 @@
 #ifndef STACK_HPP
 #define STACK_HPP
 
+//       Stack -> 클래스 템플릿
+// Stack<TYPE> -> 클래스
+
+template <typename TYPE>
+class Stack {
+private:
+    TYPE* buff;
+    int top;
+
+public:
+    ~Stack();
+    Stack(int sz = 10);
+
+    void push(TYPE n);
+    TYPE pop();
+};
+
+template <typename TYPE>
+Stack<TYPE>::~Stack()
+{
+    delete[] buff;
+}
+
+template <typename TYPE>
+Stack<TYPE>::Stack(int sz)
+{
+    top = 0;
+    buff = new TYPE[sz];
+}
+
+template <typename TYPE>
+void Stack<TYPE>::push(TYPE n)
+{
+    buff[top++] = n;
+}
+
+template <typename TYPE>
+TYPE Stack<TYPE>::pop()
+{
+    return buff[--top];
+}
+
+#if 0
 template <typename TYPE>
 class Stack {
 private:
@@ -30,5 +73,6 @@ public:
         return buff[--top];
     }
 };
+#endif
 
 #endif

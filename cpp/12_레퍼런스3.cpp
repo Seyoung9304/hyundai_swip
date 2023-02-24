0db9de37806c3dd3ba2f90bd05f6ba25dea345b6
Chansik Yun <chansigi@gmail.com>
2023-02-24 11:14:23 +0900

    .

--- cpp/12_레퍼런스3.cpp
+++ cpp/12_레퍼런스3.cpp
@@ -18,28 +18,45 @@ struct Complex {
     int im;
 };
 
-void printComplex(const Complex& r)
+void PrintComplex1(Complex c)
 {
-    // r.re = 100; /* 컴파일 에러 */
-    cout << r.re << ", " << r.im << endl;
+    cout << c.re << ", " << c.im << endl;
 }
 
-void printComplex(const Complex* p)
+// C Style
+void PrintComplex2(const Complex* p)
 {
-    if (!p) {
-        return;
+    if (p) {
+        cout << p->re << ", " << p->im << endl;
     }
 
-    cout << p->re << ", " << p->im << endl;
+    // p->im = 100;
+}
+
+// C++ Style
+void PrintComplex3(const Complex& c)
+{
+    cout << c.re << ", " << c.im << endl;
+
+    // c.re = 100;
+}
+
+int& foo()
+{
+    int n = 100;
+    // return &n;
+
+    return n;
 }
 
 int main()
 {
-    printComplex(NULL);
-}
+    int& r = foo();
+    r = 200; /* 이미 사라진 메모리에 대한 접근이 발생입니다. */
+    // 미정의 동작
 
-#if 0
-void printComplex(Complex c)
-{
+    Complex c1 { 10, 20 };
+
+    PrintComplex1(c1);
+    PrintComplex2(&c1);
 }
-#endif

18ee5d51dbb67745e52067660666c7427024d49e
Chansik Yun <chansigi@gmail.com>
2023-02-27 13:21:32 +0900

    .

--- cpp/22_복사생성자.cpp
+++ cpp/22_복사생성자.cpp
@@ -8,7 +8,7 @@ using namespace std;
 // 2. 사용자가 자신과 동일한 타입을 인자를 받는 생성자(복사 생성자)를
 //    제공하지 않으면, 컴파일러는 자동으로 멤버 데이터를 복사하는
 //    복사 생성자를 제공합니다.
-
+#if 0
 class Point {
     int x;
     int y;
@@ -47,3 +47,16 @@ int main()
     Point pt4 { pt };
     Point pt5 = { pt };
 }
+#endif
+
+class Sample1 {
+};
+// 기본생성자, 복사생성자
+
+class Sample2 {
+public:
+    Sample2(const Sample2&) { }
+}
+// 복사생성자(사용자), 기본 생성자는 제공되지 않습니다.
+// => 3. 사용자가 복사 생성자를 제공하면, 기본 생성자는 제공되지 않습니다.
+//   "복사생성자"도 생성자이기 때문입니다.

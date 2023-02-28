9577cb6bb6ccdf563538959c2b27485df21d4f8c
Chansik Yun <chansigi@gmail.com>
2023-02-28 11:09:40 +0900

    .

--- cpp/28_상속2.cpp
+++ cpp/28_상속2.cpp
@@ -14,6 +14,7 @@ class Cat {
 };
 #endif
 
+#if 0
 class Animal {
     int age;
 };
@@ -30,4 +31,24 @@ int main()
 {
     cout << sizeof(Dog) << endl;
     cout << sizeof(Cat) << endl;
+}
+#endif
+
+// 부모 클래스, Base, Super
+class Animal {
+    int age;
+};
+
+// C++ 상속
+//  : class 자식클래스 : public 부모클래스
+
+// 자식 클래스, Derived, Sub
+class Dog : public Animal {
+    int color;
+};
+
+int main()
+{
+    cout << sizeof(Animal) << endl;
+    cout << sizeof(Dog) << endl;
 }

1b5a8871f3318be02c47f176dd231c2c769bdf0e
Chansik Yun <chansigi@gmail.com>
2023-03-02 14:05:53 +0900

    .

--- cpp/34_예외3.cpp
+++ cpp/34_예외3.cpp
@@ -14,8 +14,24 @@ void foo(int n)
 #endif
 
 // 1. C++은 예외의 종류에 따라서 사용자 정의 타입 예외를 정의합니다.
-class InvalidFilenameException { };
-class OutOfMemoryError { };
+// class InvalidFilenameException { };
+// class OutOfMemoryError { };
+
+// 2. std::exception 상속해서, 에러를 정의합니다.
+class InvalidFilenameException : public std::exception {
+public:
+    const char* what() const noexcept override
+    {
+        return "잘못된 파일명입니다.";
+    }
+};
+class OutOfMemoryError : public std::exception {
+public:
+    const char* what() const noexcept override
+    {
+        return "메모리 부족합니다.";
+    }
+};
 
 void foo(int n)
 {
@@ -28,6 +44,12 @@ void foo(int n)
 
 int main()
 {
+    try {
+        foo(1);
+    } catch (std::exception& e) {
+        cerr << e.what() << endl;
+    }
+
     try {
         foo(1);
     } catch (OutOfMemoryError& e) {
